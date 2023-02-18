e5d8138d2939d09e0c816e0091e17a6443b5cd3b
Michael Bourne <michaelbourne@users.noreply.github.com>
2023-02-17 19:58:35 -0700

    Delete user-configs.php

--- user-configs.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * This file is included in the wp-config.php file.
- * Users can add their own WordPress configurations here
- */
-define( 'GRAVITYZWR_ACCOUNT_ID', '9BvAQTUJQ5K_r9Plm-2I5Q' );
-define( 'GRAVITYZWR_CLIENT_ID', 's4wlWuoTR5CvGOg4KXyzXQ' );
-define( 'GRAVITYZWR_CLIENT_SECRET', '7jRK2fJsCpD47tgO2213NGDP1R1BVfVB' );
